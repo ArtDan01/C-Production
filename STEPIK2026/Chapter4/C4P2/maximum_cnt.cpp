2dda8a66a1ac02aea968531b11e1b1c71d9f59f1
Artem Danilov <danilov@intsys.space>
2026-03-06 17:07:39 +0300

    a lil but constant!

--- STEPIK2026/Chapter4/C4P2/maximum_cnt.cpp
+++ STEPIK2026/Chapter4/C4P2/maximum_cnt.cpp
@@ -3,20 +3,46 @@ using namespace std;
 int main() {
     // put your code here
     int number, max;
-    int cnt = 0;
+    int cnt = 1;
     cin >> number;
-    if(number == 0){cout << "0" << endl;return 0;}
+    if(number == 0){cnt = 0; cout << cnt << endl; return 0;}
     max = number;
     do{
         cin >> number;
+        if(number == max) cnt ++;
         if(number > max) {
             max = number;
             cnt = 1;
         }
-        if(number == max) cnt ++;
 
     } while(number != 0);
 
     cout << cnt << endl;
     return 0;
 }
+
+/*
+Вариант преподавателя
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number, max = 0; //поскольку числа неотрицательные, любое будет больше или равно 0
+    int count = 0;
+    while (true) { //бесконечный цикл
+        cin >> number;
+        if (number == 0) break;
+        if (number > max) {
+            max = number;
+            count = 1; //один максимум уже есть
+            continue; //чтобы избежать следующих проверок, заворачиваем на новую итерацию
+        }
+        if (number == max) {
+            count++;
+        }
+    }
+    cout << count;
+    return 0;
+}
+*/
