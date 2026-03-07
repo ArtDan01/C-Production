da2730fe2e19fa97ba6f790efe8d0d1f57c9f14c
Artem <artemdan01@rambler.ru>
2026-03-07 13:05:20 +0300

    ..

---
+++ STEPIK2026/Chapter4/C4P4/upsided_down_triangle.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // put your code here
+    int h;
+    cin >> h;
+    int zvezda, probel;
+    for(int i = 0; i < h; ++i){
+        for(int j = 1; j <
+    }
+    return 0;
+}
