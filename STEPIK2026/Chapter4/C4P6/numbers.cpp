10fa6b2564ac2d054bbefe745eceb9111822855a
Artem <artemdan01@rambler.ru>
2026-03-15 23:29:31 +0300

    mac prepull

--- STEPIK2026/Chapter4/C4P6/numbers.cpp
+++ STEPIK2026/Chapter4/C4P6/numbers.cpp
@@ -2,15 +2,16 @@
 using namespace std;
 int main() {
     // put your code here
-    int start = 100;
+    /*
+    ! Внутренний цикл выполняется полностью на одну итерацию внешнего
+    ! значит внешний должен итерироваться по строкам, а внутренний по элементам от 1 до степа
+    */
     int step, columns;
+    int start = 100;
     cin >> step >> columns;
-    int lines = 900 / (step * columns);
-    if(900 % (step * columns != 0)) lines ++;
-    for(int i = 1; i <= lines; i++){
-        for(int j = 1; j <= columns; j++){
-            cout << start << ' ';
-            start += step;
+    for(int i = 1; i <= columns; i++){
+        for(int j = start; j <=start +step*columns; j+=step){
+            cout << j << ' ';
         }
         cout << endl;
     }
