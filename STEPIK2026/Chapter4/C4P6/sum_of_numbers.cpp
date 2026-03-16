d90ba10a55f29e19de3720e6dd3f7a43e762d730
Artem Danilov <danilov@intsys.space>
2026-03-16 14:49:25 +0300

    2 numbers to do

--- STEPIK2026/Chapter4/C4P6/sum_of_numbers.cpp
+++ STEPIK2026/Chapter4/C4P6/sum_of_numbers.cpp
@@ -1,27 +1,51 @@
 #include <iostream>
 using namespace std;
+/*
+! Решить!!!
+*/
 int main() {
     // put your code here
-    int input;
-    cin >> input;
-    int max = input;
-    int sum = 0;
-    while (input != 0){
-        cin >> input;
-        while(input / 10 != 0){
-            if(input % 10 == 0){
-                input /= 10;
-                continue;
-            } else {
-                sum+=input%10;
-                input /10; 
-            }
-            if(input / 10 == 0){
-                if (input % 10 == 0) break;
-                else sum += input%10; break;
+    int local_max;
+    cin >> local_max;
+    int max_sum = 0;
+    int tmp = local_max;
+    if(local_max < 10){
+        max_sum = local_max;
+    } else {
+        while(true){
+            max_sum += local_max % 10;
+            local_max /= 10;
+            if(local_max / 10 == 0){
+                max_sum += local_max;
+                local_max = tmp;
+                break;
             }
         }
     }
-    cout << sum << endl;
+    int number;
+    int sum;
+    int loc_tmp;
+    while(number != 0){
+        sum = 0;
+        cin >> number;
+        loc_tmp = number;
+        if(number < 10){
+        sum = number;
+    } else {
+        while(true){
+            sum += number % 10;
+            number /= 10;
+            if(number / 10 == 0){
+                sum += number;
+                break;
+            }
+        }
+    }
+    if(sum > max_sum){
+        max_sum = sum;
+        local_max = loc_tmp;
+    }
+    }
+    cout << local_max << ' ' << max_sum << endl; 
     return 0;
 }
