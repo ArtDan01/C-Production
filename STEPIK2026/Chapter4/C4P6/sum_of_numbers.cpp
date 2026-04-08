8a3ef0e8d5dd93a136aaa2f7676731802462eb74
Artem <artemdan01@rambler.ru>
2026-04-08 23:02:58 +0300

    arrays start, i'm back, stay tuned & focused

--- STEPIK2026/Chapter4/C4P6/sum_of_numbers.cpp
+++ STEPIK2026/Chapter4/C4P6/sum_of_numbers.cpp
@@ -5,39 +5,23 @@ using namespace std;
 */
 int main() {
     // put your code here
-    int local_max;
-    cin >> local_max;
-    int max_sum = 0;
-    int tmp = local_max;
-    if(local_max < 10){
-        max_sum = local_max;
-    } else {
-        while(true){
-            max_sum += local_max % 10;
-            local_max /= 10;
-                if(local_max / 10 == 0){
-                    max_sum += local_max;
-                    local_max = tmp;
-                    break;
-                }
-        }
-    }
-    int number;
-    int sum;
-    int loc_tmp;
-    while(number != 0){
-        sum = 0;
-        cin >> number;
-        loc_tmp = number;
-        if(number < 10){
-        sum = number;
-    } else {
-        while(true){
-            sum += number % 10;
-            number /= 10;
-            if(number / 10 == 0){
-                sum += number;
-                break;
+    int input;
+    cin >> input;
+    int max = input;
+    int sum = 0;
+    while (input != 0){
+        cin >> input;
+        while(input / 10 != 0){
+            if(input % 10 == 0){
+                input /= 10;
+                continue;
+            } else {
+                sum+=input%10;
+                input /10; 
+            }
+            if(input / 10 == 0){
+                if (input % 10 == 0) break;
+                else sum += input%10; break;
             }
         }
     }
